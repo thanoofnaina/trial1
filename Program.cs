3acfa9f647bae1d499473504f9f5869eb3ba25eb
T N Thanoof Naina <thanoof.tn@experionglobal.local>
2023-02-13 14:57:02 +0530

    first

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int num1 = 1;
+int num2 = 2;
+int temp;
+
+temp = num2;
+num2 = num1;
+num1 = temp;
+
+Console.WriteLine($"{num2}, {num1} is the orginal numbers.");
+Console.WriteLine($"{num1}, {num2} is the reversed numbers.");
